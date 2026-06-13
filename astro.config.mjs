a1475314085778d48bbca917609416dfd880c5a9
Boluwatife <holarbolu@gmail.com>
2026-06-13 23:13:02 +0100

    Add GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://quantumhub.me',
-  output: 'static',
+  site: 'https://boluadurosakin.github.io',
+  base: '/quantumhub',
 });
