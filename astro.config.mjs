d3e106e27f4862e59124c16a31bc5efe84fed6ba
Boluwatife <holarbolu@gmail.com>
2026-06-14 15:44:03 +0100

    Fix custom domain config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://boluadurosakin.github.io',
-  base: '/quantumhub',
+  site: 'https://quantumhub.me',
 });
